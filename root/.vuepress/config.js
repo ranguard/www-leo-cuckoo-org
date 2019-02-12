ec9cdddf617eaae838f5defd98457cba6ab8781a
Leo Lapworth <leo@cuckoo.org>
2019-02-12 20:46:23 +0000

    add GA

--- root/.vuepress/config.js
+++ root/.vuepress/config.js
@@ -1,17 +1,18 @@
 module.exports = {
-  title: 'Leo Lapworth',
-  description: 'Technologist, developer and manager',
-    themeConfig: {
-      sidebar: 'auto',
-      sidebarDepth: 5,
-      displayAllHeaders: true,
-    	lastUpdated: 'Last Updated',
+  title: "Leo Lapworth",
+  ga: "UA-1820505-1",
+  description: "Technologist, developer and manager",
+  themeConfig: {
+    sidebar: "auto",
+    sidebarDepth: 5,
+    displayAllHeaders: true,
+    lastUpdated: "Last Updated",
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'CV', link: '/cv/' },
-      { text: 'Open Source', link: '/opensource/' },
-      { text: 'Talks', link: '/talks/' },
-      { text: 'Projects', link: '/projects/' }
+      { text: "Home", link: "/" },
+      { text: "CV", link: "/cv/" },
+      { text: "Open Source", link: "/opensource/" },
+      { text: "Talks", link: "/talks/" },
+      { text: "Projects", link: "/projects/" }
     ]
   }
-}
+};
